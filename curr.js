90d4448786c45a8f15e91ec28f536e4eb3cd1eed
tgaherwar <tanay_tgs@yahoo.co.in>
2015-07-15 16:02:22 +0530

    final versions

--- curr.js
+++ curr.js
@@ -1,10 +1,37 @@
+.pragma library
 
-function readapi(a,b)
+function readapi(options)
 {
-    a= decipher(a);
-    b= decipher(b);
-    var url="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fd%2Fquotes.csv%3Fe%3D.csv%26f%3Dc4l1%26s%3D"+a+b+"%3DX%22%3B&format=json&diagnostics=true&callback=";
+    if(options.first && options.second)
+    {
+        options.first = decipher(options.first);
+        options.second = decipher(options.second);
+        var urlink ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fd%2Fquotes.csv%3Fe%3D.csv%26f%3Dc4l1%26s%3D"+options.first+options.second+"%3DX%22%3B&format=json&diagnostics=true&callback=";
 
+        var doc = new XMLHttpRequest();
+        doc.onreadystatechange=function()
+        {
+            if(doc.readyState === XMLHttpRequest.DONE)
+            {
+                if(doc.status === 200)
+                {
+                    var re = doc.responseText;
+                    if(re)
+                    {
+                        var conversion = JSON.parse(re);
+                        if(options.success) options.success(parseInt(conversion.query.results.row.col1));
+                    }
+                } else
+                {
+                    if(options.error) options.error(doc.status + " " + doc.statusText);
+                }
+            }
+        }
+
+        doc.open("get" , urlink);
+        doc.setRequestHeader("Content-Encoding", "UTF-8");
+        doc.send();
+    }
 }
 
 function decipher(a)
@@ -17,7 +44,7 @@ function decipher(a)
     {
         return ("USD");
     }
-    if(a.toUpperCase() === "RUPPEE"||a.toUpperCase() === "INR")
+    if(a.toUpperCase() === "RUPEE"||a.toUpperCase() === "INR")
     {
         return ("INR");
     }
